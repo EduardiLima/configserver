12f939bcad410f1e06536da60d4c990e867f4304
EduardiLima <dudulimaxt54@gmail.com>
2023-08-20 11:47:09 -0300

    first commit

---
+++ src/test/java/br/com/tech4m/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tech4m.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
